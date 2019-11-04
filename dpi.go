137c68a043777f4ce63fa0ecab96b6ec0a313d0e
0xhjohnson <0xhjohnson@protonmail.com>
2019-11-04 15:49:00 -0500

    Outsourced res parsing and migrated to named returns for readability

--- dpi.go
+++ dpi.go
@@ -24,27 +24,42 @@ func getPlatform() string {
 	return runtime.GOOS
 }
 
-func getDisplays() string {
+func getDisplays() (out string) {
 	cmd := "xrandr | grep -w connected"
-	out, err := exec.Command("bash", "-c", cmd).Output()
+	o, err := exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
 		log.Fatalf("Error running xrandr command: %v", err)
 	}
-	return string(out)
+	out = string(o)
+	return
 }
 
-func toInches(sz string) float64 {
+func toInches(sz string) (in float64) {
 	s := strings.ReplaceAll(sz, "mm", "")
 	i, err := strconv.Atoi(s)
 	if err != nil {
 		fmt.Printf("Failed to parse size in mm: %v", err)
 	}
 	cm := i / 10
-	in := float64(cm) / 2.54
-	return in
+	in = float64(cm) / 2.54
+	return
 }
 
-func getDPI(res []float64, dim []float64) (dpi int) {
+func parseRes(re string) (res []float64) {
+	res = make([]float64, 0, 2)
+	sp := strings.Split(re, "+")[0]
+	spp := strings.Split(sp, "x")
+	for _, v := range spp {
+		f, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			fmt.Printf("Failed to parse resolution: %v\n", err)
+		}
+		res = append(res, f)
+	}
+	return
+}
+
+func calcDPI(res []float64, dim []float64) (dpi int) {
 	ds := make([]float64, 0, 2)
 	// ensure calculation works independent of monitor orientation
 	sort.Float64s(res)
@@ -67,30 +82,24 @@ func main() {
 	screens := getDisplays()
 	s := bufio.NewScanner(strings.NewReader(screens))
 	s.Split(bufio.ScanWords)
+
 	var name string
-	res := make([]float64, 0, 2)
+	var res []float64
 	dim := make([]float64, 0, 2)
+
 	for s.Scan() {
 		switch s.Text() {
 		case "DVI-D-0", "HDMI-0", "DP-0", "DP-1", "DP-2", "DP-3", "DP-4", "DP-5":
 			name = s.Text()
 		}
 		if strings.HasSuffix(s.Text(), "+0") {
-			sp := strings.Split(s.Text(), "+")[0]
-			spp := strings.Split(sp, "x")
-			for _, v := range spp {
-				f, err := strconv.ParseFloat(v, 64)
-				if err != nil {
-					fmt.Printf("Failed to parse resolution: %v\n", err)
-				}
-				res = append(res, f)
-			}
+			res = parseRes(s.Text())
 		}
 		if strings.HasSuffix(s.Text(), "mm") {
 			in := toInches(s.Text())
 			dim = append(dim, in)
 			if len(dim) == 2 {
-				dpi := getDPI(res, dim)
+				dpi := calcDPI(res, dim)
 				p := display{name, res, dim, dpi}
 				fmt.Printf("%s dpi: %d \n", p.name, p.dpi)
 				// reset in prep for multiple displays
